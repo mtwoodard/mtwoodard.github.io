6d8bca89119a174b8fef330a28b110928b51d269
Michael Woodard <1995mtwoodard@gmail.com>
2018-04-17 11:49:05 -0500

    Getting rid of an output variable

--- debugMar13-4/shaders/fragment.glsl
+++ debugMar13-4/shaders/fragment.glsl
@@ -8,7 +8,7 @@ vec4 getRay(float fov, vec2 resolution, vec2 fragCoord){
 
 float raymarchDistance(vec4 rO, vec4 rD, float start, float end, out vec4 localEndPoint,
   out vec4 globalEndPoint, out vec4 localEndTangentVector, out vec4 globalEndTangentVector,
-  out mat4 totalFixMatrix, out float tilingSteps, out int hitWhich){
+  out mat4 totalFixMatrix, out int hitWhich){
   int fakeI = 0;
   float globalDepth = start;
   float localDepth = globalDepth;
@@ -25,7 +25,6 @@ float raymarchDistance(vec4 rO, vec4 rD, float start, float end, out vec4 localE
     vec4 localSamplePoint = pointOnGeodesic(localrO, localrD, localDepth);
     vec4 globalSamplePoint = pointOnGeodesic(rO, rD, globalDepth);
     if(isOutsideCell(localSamplePoint, fixMatrix)){
-      tilingSteps++;
       totalFixMatrix *= fixMatrix;
       vec4 newDirection = pointOnGeodesic(localrO, localrD, localDepth + 0.1); //forwards a bit
       localrO = localSamplePoint*fixMatrix;
@@ -90,7 +89,7 @@ void main(){
   //get our raymarched distance back ------------------------
   float dist = raymarchDistance(rayOrigin, rayDirVPrime, MIN_DIST, MAX_DIST, localEndPoint,
     globalEndPoint, localEndTangentVector, globalEndTangentVector, totalFixMatrix,
-    tilingSteps, hitWhich);
+    hitWhich);
   if(hitWhich == 0){ //Didn't hit anything ------------------------
     vec4 pointAtInfinity = pointOnGeodesicAtInfinity(rayOrigin, rayDirVPrime) * cellBoost;  //cellBoost corrects for the fact that we have been moving through cubes
     gl_FragColor = vec4(0.5*normalize(pointAtInfinity.xyz)+vec3(0.5,0.5,0.5),1.0);
@@ -111,7 +110,7 @@ void main(){
     //float shineShade = lorentzDot(localSurfaceNormal, directionToLightSource);
 
     float depthShade = max(1.0-dist/5.0, 0.0);
-    float stepsShade = max(1.0-tilingSteps/3.0,0.0);
+    //float stepsShade = max(1.0-tilingSteps/3.0,0.0);
     // float comboShade = shineShade*depthShade;
     vec4 depthColor = vec4(depthShade,depthShade*0.65,0.1,1.0);
     // vec4 stepsColor = vec4(stepsShade,stepsShade,stepsShade,1.0);
