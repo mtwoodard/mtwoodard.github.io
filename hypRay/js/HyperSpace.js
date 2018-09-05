625f40b78d537e00850dfd1deece16baf80e6d72
mtwoodard <1995mtwoodard@gmail.com>
2018-09-05 17:04:46 -0500

    Trying to get a non-VR version working.

---
+++ hypRay/js/HyperSpace.js
@@ -0,0 +1,138 @@
+//-------------------------------------------------------
+// Global Variables
+//-------------------------------------------------------
+var g_fov;
+var g_material;
+var g_controls;
+var g_rotation;
+var g_currentBoost;
+var g_cellBoost;
+var g_invCellBoost;
+var g_screenResolution;
+
+//-------------------------------------------------------
+// Scene Variables
+//-------------------------------------------------------
+var scene;
+var renderer;
+var camera;
+var mesh;
+var geom;
+var maxSteps = 50;
+var time;
+
+
+//-------------------------------------------------------
+// Sets up precalculated values
+//-------------------------------------------------------
+var hCWH = 0.6584789485;
+var gens;
+var invGens;
+
+var initValues = function(){
+	gens = createGenerators();
+  invGens = invGenerators(gens);
+}
+
+var createGenerators = function(){
+  var gen0 = translateByVector(new THREE.Vector3(2.0*hCWH,0.0,0.0));
+  var gen1 = translateByVector(new THREE.Vector3(-2.0*hCWH,0.0,0.0));
+  var gen2 = translateByVector(new THREE.Vector3(0.0,2.0*hCWH,0.0));
+  var gen3 = translateByVector(new THREE.Vector3(0.0,-2.0*hCWH,0.0));
+  var gen4 = translateByVector(new THREE.Vector3(0.0,0.0,2.0*hCWH));
+  var gen5 = translateByVector(new THREE.Vector3(0.0,0.0,-2.0*hCWH));
+  return [gen0, gen1, gen2, gen3, gen4, gen5];
+}
+
+var invGenerators = function(genArr){
+  return [genArr[1],genArr[0],genArr[3],genArr[2],genArr[5],genArr[4]];
+}
+
+
+//-------------------------------------------------------
+// Sets up the lights
+//-------------------------------------------------------
+var lightPositions = [];
+var lightIntensities = [];
+var attnModel = 1;
+
+var initLights = function(){
+  PointLightObject(new THREE.Vector3(1.2,0,0), new THREE.Vector4(1,0,0,1));
+  PointLightObject(new THREE.Vector3(0,1.2,0), new THREE.Vector4(0,1,0,1));
+  PointLightObject(new THREE.Vector3(0,0,1.2), new THREE.Vector4(0,0,1,1));
+  PointLightObject(new THREE.Vector3(-1,-1,-1), new THREE.Vector4(1,1,1,1));
+}
+
+//-------------------------------------------------------
+// Sets up global objects
+//-------------------------------------------------------
+var globalObjectBoost;
+
+var initObjects = function(){
+  globalObjectBoost = new THREE.Matrix4().multiply(translateByVector(new THREE.Vector3(-0.5,0,0)));
+}
+
+//-------------------------------------------------------
+// Sets up the scene
+//-------------------------------------------------------
+var init = function(){
+  //Setup our THREE scene--------------------------------
+	time = Date.now();
+  scene = new THREE.Scene();
+  renderer = new THREE.WebGLRenderer();
+  camera = new THREE.OrthographicCamera(-1,1,1,-1,1/Math.pow(2,53),1);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+  g_controls = new THREE.Controls();
+  g_rotation = new THREE.Quaternion();
+  g_currentBoost = new THREE.Matrix4(); // boost for camera relative to central cell
+  g_cellBoost = new THREE.Matrix4(); // boost for the cell that we are in relative to where we started
+  g_invCellBoost = new THREE.Matrix4();
+	initValues();
+  initLights();
+  initObjects();
+  g_material = new THREE.ShaderMaterial({
+    uniforms:{
+      screenResolution:{type:"v2", value:g_screenResolution},
+      invGenerators:{type:"m4v", value:invGens},
+      currentBoost:{type:"m4", value:g_currentBoost},
+      cellBoost:{type:"m4", value:g_cellBoost},
+      invCellBoost:{type:"m4", value:g_invCellBoost},
+			lightPositions:{type:"v4v", value:lightPositions},
+      lightIntensities:{type:"v3v", value:lightIntensities},
+      globalObjectBoost:{type:"m4", value:globalObjectBoost}    
+    },
+    vertexShader: document.getElementById('vertexShader').textContent,
+    fragmentShader: document.getElementById('fragmentShader').textContent,
+    transparent:true
+  });
+  //Setup a "quad" to render on-------------------------
+  geom = new THREE.BufferGeometry();
+  var vertices = new Float32Array([
+    -1.0, -1.0, 0.0,
+     1.0, -1.0, 0.0,
+     1.0,  1.0, 0.0,
+
+    -1.0, -1.0, 0.0,
+     1.0,  1.0, 0.0,
+    -1.0,  1.0, 0.0
+  ]);
+  geom.addAttribute('position',new THREE.BufferAttribute(vertices,3));
+  mesh = new THREE.Mesh(geom, g_material);
+  scene.add(mesh);
+  animate();
+}
+
+//-------------------------------------------------------
+// Where our scene actually renders out to screen
+//-------------------------------------------------------
+var animate = function(){
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  g_controls.update();
+}
+
+//-------------------------------------------------------
+// Where the magic happens
+//-------------------------------------------------------
+init();
