d5ec87aa48a8bb7d843120bc2c23ff24a90dcde4
Michael Woodard <1995mtwoodard@gmail.com>
2018-09-24 20:04:39 -0500

    Testing out to see if lowp was causing any issues

--- hypBase/shaders/raymarch.glsl
+++ hypBase/shaders/raymarch.glsl
@@ -9,71 +9,71 @@ BEGIN FRAGMENT
   //--------------------------------------------
   //Global Constants
   //--------------------------------------------
-  const lowp int MAX_MARCHING_STEPS = 32;
-  const lowp float MIN_DIST = 0.0;
-  const lowp float MAX_DIST = 100.0;
-  const lowp float EPSILON = 0.0001;
-  const lowp float fov = 90.0;
-  const lowp float horosphereSize = -0.951621;
-  const lowp float sphereRad = 0.996216;
-  const lowp float halfCubeWidthKlein = 0.5773502692;
-  const lowp float globalObjectRadius = 0.2;
-  const lowp vec4 ORIGIN = vec4(0,0,0,1);
+  const int MAX_MARCHING_STEPS = 32;
+  const float MIN_DIST = 0.0;
+  const float MAX_DIST = 100.0;
+  const float EPSILON = 0.0001;
+  const float fov = 90.0;
+  const float horosphereSize = -0.951621;
+  const float sphereRad = 0.996216;
+  const float halfCubeWidthKlein = 0.5773502692;
+  const float globalObjectRadius = 0.2;
+  const vec4 ORIGIN = vec4(0,0,0,1);
   //--------------------------------------------
   //Generated Constants
   //--------------------------------------------
-  const lowp float halfIdealCubeWidthKlein = 0.5773502692;
-  const lowp vec4 idealCubeCornerKlein = vec4(halfIdealCubeWidthKlein, halfIdealCubeWidthKlein, halfIdealCubeWidthKlein, 1.0);
+  const float halfIdealCubeWidthKlein = 0.5773502692;
+  const vec4 idealCubeCornerKlein = vec4(halfIdealCubeWidthKlein, halfIdealCubeWidthKlein, halfIdealCubeWidthKlein, 1.0);
   //-------------------------------------------
   //Translation & Utility Variables
   //--------------------------------------------
-  uniform lowp vec2 screenResolution;
-  uniform lowp mat4 invGenerators[6];
-  uniform lowp mat4 currentBoost;
-  uniform lowp mat4 cellBoost; 
-  uniform lowp mat4 invCellBoost;
+  uniform vec2 screenResolution;
+  uniform mat4 invGenerators[6];
+  uniform mat4 currentBoost;
+  uniform mat4 cellBoost; 
+  uniform mat4 invCellBoost;
   //--------------------------------------------
   //Lighting Variables & Global Object Variables
   //--------------------------------------------
-  uniform lowp vec4 lightPositions[4];
-  uniform lowp vec4 lightIntensities[4];
+  uniform vec4 lightPositions[4];
+  uniform vec4 lightIntensities[4];
   
   //--------------------------------------------------------------------
   // Hyperbolic Functions
   //--------------------------------------------------------------------
-  lowp float cosh(lowp float x){
-    lowp float eX = exp(x);
+  float cosh(float x){
+    float eX = exp(x);
     return (0.5 * (eX + 1.0/eX));
   }
   
-  lowp float acosh(lowp float x){ //must be more than 1
+  float acosh(float x){ //must be more than 1
     return log(x + sqrt(x*x-1.0));
   }
   
-  lowp float sinh(lowp float x){
-    lowp float eX = exp(x);
+  float sinh(float x){
+    float eX = exp(x);
     return (0.5 * (eX - 1.0/eX));
   }
   
-  lowp float hypDot(lowp vec4 u, lowp vec4 v){
+  float hypDot(vec4 u, vec4 v){
     return u.x*v.x + u.y*v.y + u.z*v.z - u.w*v.w; // Lorentz Dot
   }
   
-  lowp float hypNorm(lowp vec4 v){
+  float hypNorm(vec4 v){
     return sqrt(abs(hypDot(v,v)));
   }
   
-  lowp vec4 hypNormalize(lowp vec4 u){
+  vec4 hypNormalize(vec4 u){
     return u/hypNorm(u);
   }
   
-  lowp float hypDistance(lowp vec4 u, lowp vec4 v){
-    lowp float bUV = -hypDot(u,v);
+  float hypDistance(vec4 u, vec4 v){
+    float bUV = -hypDot(u,v);
     return acosh(bUV);
   }
   
-  lowp vec4 hypDirection(lowp vec4 u, lowp vec4 v){
-    lowp vec4 w = v + hypDot(u,v)*u;
+  vec4 hypDirection(vec4 u, vec4 v){
+    vec4 w = v + hypDot(u,v)*u;
     return hypNormalize(w);
   }
   
@@ -81,11 +81,11 @@ BEGIN FRAGMENT
   //Hyperboloid Functions
   //-------------------------------------------------------
   // Get point at distance dist on the geodesic from u in the direction vPrime
-  lowp vec4 pointOnGeodesic(lowp vec4 u, lowp vec4 vPrime, lowp float dist){
+  vec4 pointOnGeodesic(vec4 u, vec4 vPrime, float dist){
     return u*cosh(dist) + vPrime*sinh(dist);
   }
   
-  lowp vec4 tangentVectorOnGeodesic(lowp vec4 u, lowp vec4 vPrime, lowp float dist){
+  vec4 tangentVectorOnGeodesic(vec4 u, vec4 vPrime, float dist){
     // note that this point has hypDot with itself of -1, so it is on other hyperboloid
     return u*sinh(dist) + vPrime*cosh(dist);
   }
@@ -96,48 +96,48 @@ BEGIN FRAGMENT
   // A horosphere can be constructed by offseting from a standard horosphere.
   // Our standard horosphere will have a center in the direction of lightPoint
   // and go through the origin. Negative offsets will shrink it.
-  float horosphereHSDF(lowp vec4 samplePoint, lowp vec4 lightPoint, lowp float offset){
+  float horosphereHSDF(vec4 samplePoint, vec4 lightPoint, float offset){
     return log(-hypDot(samplePoint, lightPoint)) - offset;
   }
   
-  lowp float sphereSDF(lowp vec4 samplePoint, lowp vec4 center, lowp float radius){
+  float sphereSDF(vec4 samplePoint, vec4 center, float radius){
     return hypDistance(samplePoint, center) - radius;
   }
   
   //---------------------------------------------------------------------
   //Scene Definitions
   //---------------------------------------------------------------------
-  lowp float localSceneSDF(lowp vec4 samplePoint){
-    lowp float sphere = sphereSDF(samplePoint, ORIGIN, sphereRad);
-    lowp float vertexSphere = 0.0;
+  float localSceneSDF(vec4 samplePoint){
+    float sphere = sphereSDF(samplePoint, ORIGIN, sphereRad);
+    float vertexSphere = 0.0;
     vertexSphere = horosphereHSDF(abs(samplePoint), idealCubeCornerKlein, horosphereSize);
-    lowp float final = -min(vertexSphere,sphere); //unionSDF
+    float final = -min(vertexSphere,sphere); //unionSDF
     return final;
   }
   
   //--------------------------------------------------------------------
   // Lighting Functions
   //--------------------------------------------------------------------
-  lowp vec3 lightingCalculations(lowp vec4 SP, lowp vec4 TLP, lowp vec4 V, lowp vec4 normal, lowp vec4 lightIntensity){
+  vec3 lightingCalculations(vec4 SP, vec4 TLP, vec4 V, vec4 normal, vec4 lightIntensity){
     //Calculations - Phong Reflection Model
-    lowp vec4 L = hypDirection(SP, TLP);
-    lowp vec4 R = 2.0*hypDot(L, normal)*normal - L;
+    vec4 L = hypDirection(SP, TLP);
+    vec4 R = 2.0*hypDot(L, normal)*normal - L;
     //Calculate Diffuse Component
-    lowp float nDotL = max(hypDot(normal, L),0.0);
-    lowp vec3 diffuse = lightIntensity.rgb * nDotL;
-    lowp //Calculate Specular Component
+    float nDotL = max(hypDot(normal, L),0.0);
+    vec3 diffuse = lightIntensity.rgb * nDotL;
+    //Calculate Specular Component
     float rDotV = max(hypDot(R, V),0.0);
-    lowp vec3 specular = lightIntensity.rgb * pow(rDotV,10.0);
+    vec3 specular = lightIntensity.rgb * pow(rDotV,10.0);
     //Attenuation - Inverse Square
-    lowp float distToLight = hypDistance(SP, TLP);
-    lowp float att = 1.0/(0.01 + lightIntensity.w * distToLight* distToLight);
+    float distToLight = hypDistance(SP, TLP);
+    float att = 1.0/(0.01 + lightIntensity.w * distToLight* distToLight);
     //Compute final color
     return att*((diffuse*vec3(1.0)) + specular);
   }
   
-  lowp vec3 phongModel(lowp vec4 samplePoint, lowp vec4 tangentVector, lowp vec4 normal, lowp mat4 totalFixMatrix){
-    lowp vec4 V = -tangentVector;
-    lowp vec3 color = vec3(0.0);
+  vec3 phongModel(vec4 samplePoint, vec4 tangentVector, vec4 normal, mat4 totalFixMatrix){
+    vec4 V = -tangentVector;
+    vec3 color = vec3(0.0);
     //--------------------------------------------
     //Lighting Calculations
     //--------------------------------------------
@@ -154,11 +154,11 @@ BEGIN FRAGMENT
   
   
   //NORMAL FUNCTIONS ++++++++++++++++++++++++++++++++++++++++++++++++++++
-  lowp vec4 estimateNormal(lowp vec4 p) { // normal vector is in tangent hyperplane to hyperboloid at p
-    lowp float newEp = EPSILON * 10.0;
-    lowp vec4 basis_x = hypNormalize(vec4(p.w,0.0,0.0,p.x));  // dw/dx = x/w on hyperboloid
-    lowp vec4 basis_y = vec4(0.0,p.w,0.0,p.y);  // dw/dy = y/denom
-    lowp vec4 basis_z = vec4(0.0,0.0,p.w,p.z);  // dw/dz = z/denom  /// note that these are not orthonormal!
+  vec4 estimateNormal(vec4 p) { // normal vector is in tangent hyperplane to hyperboloid at p
+    float newEp = EPSILON * 10.0;
+    vec4 basis_x = hypNormalize(vec4(p.w,0.0,0.0,p.x));  // dw/dx = x/w on hyperboloid
+    vec4 basis_y = vec4(0.0,p.w,0.0,p.y);  // dw/dy = y/denom
+    vec4 basis_z = vec4(0.0,0.0,p.w,p.z);  // dw/dz = z/denom  /// note that these are not orthonormal!
     basis_y = hypNormalize(basis_y - hypDot(basis_y, basis_x)*basis_x); // need to Gram Schmidt
     basis_z = hypNormalize(basis_z - hypDot(basis_z, basis_x)*basis_x - hypDot(basis_z, basis_y)*basis_y);
     return hypNormalize(
@@ -167,15 +167,15 @@ BEGIN FRAGMENT
       basis_z * (localSceneSDF(p + newEp*basis_z) - localSceneSDF(p - newEp*basis_z)));
   }
   
-  lowp vec4 getRayPoint(lowp vec2 resolution, lowp vec2 fragCoord){ //creates a point that our ray will go through
-    lowp vec2 xy = 0.2*((fragCoord - 0.5*resolution)/resolution.x);
-    lowp float z = 0.1/tan(radians(fov*0.5));
-    lowp vec4 p =  hypNormalize(vec4(xy,-z,1.0));
+  vec4 getRayPoint(vec2 resolution, vec2 fragCoord){ //creates a point that our ray will go through
+    vec2 xy = 0.2*((fragCoord - 0.5*resolution)/resolution.x);
+    float z = 0.1/tan(radians(fov*0.5));
+    vec4 p =  hypNormalize(vec4(xy,-z,1.0));
     return p;
   }
 
-  bool isOutsideCell(lowp vec4 samplePoint, out lowp mat4 fixMatrix){
-    lowp vec4 kleinSamplePoint = samplePoint/samplePoint.w; //project to klein
+  bool isOutsideCell(vec4 samplePoint, out mat4 fixMatrix){
+    vec4 kleinSamplePoint = samplePoint/samplePoint.w; //project to klein
     if(kleinSamplePoint.x > halfCubeWidthKlein){
       fixMatrix = invGenerators[0];
       return true;
@@ -203,11 +203,11 @@ BEGIN FRAGMENT
     return false;
   }
   
-  bool raymarch(lowp vec4 rO, lowp vec4 rD, out lowp vec4 samplePoint, out lowp vec4 tangent, out lowp mat4 totalFixMatrix){
-    lowp float globalDepth = MIN_DIST; lowp float localDepth = globalDepth;
-    for(lowp int i = 0; i< MAX_MARCHING_STEPS; i++){
-      lowp mat4 fixMatrix;
-      lowp vec4 endPoint = pointOnGeodesic(rO, rD, localDepth);
+  bool raymarch(vec4 rO, vec4 rD, out vec4 samplePoint, out vec4 tangent, out mat4 totalFixMatrix){
+    float globalDepth = MIN_DIST; float localDepth = globalDepth;
+    for(int i = 0; i< MAX_MARCHING_STEPS; i++){
+      mat4 fixMatrix;
+      vec4 endPoint = pointOnGeodesic(rO, rD, localDepth);
       if(isOutsideCell(endPoint, fixMatrix)){
         totalFixMatrix *= fixMatrix;
         rO = hypNormalize(endPoint*fixMatrix);
@@ -215,7 +215,7 @@ BEGIN FRAGMENT
         localDepth = MIN_DIST;
       }
       else{
-        lowp float dist = localSceneSDF(endPoint); 
+        float dist = localSceneSDF(endPoint); 
         if(dist < EPSILON){
           //Pass information out to global variables
           samplePoint = endPoint; //local sample point
@@ -232,22 +232,22 @@ BEGIN FRAGMENT
   
   void main(){
     //get intial origin and ray direction -------------------------------------
-    lowp vec4 rayOrigin = ORIGIN;
-    lowp vec4 rayDirV = getRayPoint(screenResolution, gl_FragCoord.xy);
+    vec4 rayOrigin = ORIGIN;
+    vec4 rayDirV = getRayPoint(screenResolution, gl_FragCoord.xy);
     //camera position must be translated in hyperboloid -----------------------
     rayOrigin *= currentBoost;
     rayDirV *= currentBoost;
     //generate direction then transform to hyperboloid ------------------------
-    lowp vec4 rayDirVPrime = hypDirection(rayOrigin, rayDirV);
+     vec4 rayDirVPrime = hypDirection(rayOrigin, rayDirV);
     //get our raymarch info ---------------------------------------------------
-    lowp mat4 totalFixMatrix = mat4(1.0);
-    lowp vec4 samplePoint; lowp vec4 tangent;
+    mat4 totalFixMatrix = mat4(1.0);
+    vec4 samplePoint; vec4 tangent;
     bool hit = raymarch(rayOrigin, rayDirVPrime, samplePoint, tangent, totalFixMatrix);
     //if we hit something color it --------------------------------------------
     if(!hit) gl_FragColor = vec4(0.0);
     else{
-      lowp vec4 normal = estimateNormal(samplePoint);
-      lowp vec3 color = phongModel(samplePoint, tangent, normal, totalFixMatrix);
+      vec4 normal = estimateNormal(samplePoint);
+      vec3 color = phongModel(samplePoint, tangent, normal, totalFixMatrix);
       gl_FragColor = vec4(color, 1.0);
     }
   }
